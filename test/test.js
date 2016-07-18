c5a57f59440e9d4b548a6094f28c4c0e17d21ff2
trong-le <trong.p.le.92@gmail.com>
2016-07-18 11:20:51 -0400

    refactored code

--- test/test.js
+++ test/test.js
@@ -3,8 +3,6 @@ import TypeFormSource from '../lib/index.js';
 import { config } from './test.config';
 import fs from 'fs';
 
-
-// example - make sure configuration is the same
 test('proper configuration', t => {
 	const activity = new TypeFormSource();
 	t.equal(TypeFormSource.props.name, require('../package.json').name);
@@ -12,7 +10,9 @@ test('proper configuration', t => {
 	t.end();
 });
 
-test('oauth onTaskCopy', async (t) => {
-	const source = new TypeFormSource();
-	const data = await source.onTaskCopy(config);
+test('test api', async t => {
+    const activity = new TypeFormSource();
+    const data = await activity.getTypeformData(config);
+    const questionAnswersArray = activity.getResponses(data);
 });
+
