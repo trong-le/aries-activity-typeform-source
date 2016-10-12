c71661de2094a841e96ee04853a00d5252df9266
trong-le <trong.p.le.92@gmail.com>
2016-10-12 13:16:42 -0400

    Fixed non-adding multiple questions, removed/added indexing

--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
 import test from 'blue-tape';
 import TypeFormSource from '../lib/index.js';
 import config from './test-config';
-import fs from 'fs';
 
 test('proper configuration', t => {
 	const activity = new TypeFormSource();
